5ffef075ac4b06a0a7dc8fbf09d3d9194fd34731
Merenalis <rostikboy2001@gmail.com>
2021-09-21 20:25:16 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Main from "./components/Main"
+import Result from "./components/Result";
+import {Provider} from "react-redux";
+import store from "./store/store";
+import ModalStart from "./components/ModalStart";
+import background2 from './images/background2.png'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+    return (
+        <Provider store={store} >
+
+            <Main/>
+            <ModalStart/>
+        </Provider>
   );
+
 }
 
 export default App;
